550e87f7902360e65673d5404e91176068383eb0
Mzwandile Mahlangu <mzwandile.mahlangu@entelect.co.za>
2020-10-23 20:13:11 +0200

    added new variables to communicate with c++

--- main.cpp
+++ main.cpp
@@ -7,6 +7,30 @@
 
 using namespace emscripten;
 
+bool isCompleteLine(val board_values, val object_block) {
+    //go through all the columns in the last row and check if they have an object
+    for (int col = 0; col < 10; col++) {
+        if(board_values[19][col] != object_block) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool isGameOver(val board_values, val empty_block) {
+     //go through all the columns in the first row and check if they have an object
+    for (int col = 0; col < 10; col++) {
+        if(board_values[0][col] != empty_block) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool generateTetrimino() {
+    return true;
+}
+
 val tetris() {
     printf("MyFunction Called\n");
     val board_values = val::global("BoardValues");
@@ -14,62 +38,22 @@ val tetris() {
     val empty_block = val::global("emptyBlock");
     val object_block = val::global("objectBlock");
 
-    // for (int row = 0; row < 10; row++) {
-    //     for (int col = 0; col < 10; col++) {
-    //         board_values[row][col] = val("0");
-    //         printf("awe");
-    //     }
-    // }
+    val collapse_board = val::global("collapseBoardLabel");
+    val generate_piece = val::global("generatePieceLabel");
+    val game_over = val::global("gameOverLabel");
+    return no_winner_label;
 
-    // board_values[0][5] = val("1");
-    // int tetrimino[20][10];
-    
-    // for (int row = 0; row < 20; row++) {
-    //     for (int col = 0; col < 10; col++) {
-    //         board_values[row][col] = empty_block;
-    //     }
-    // }
+    if (isCompleteLine(board_values, object_block)) {
+       return collapse_board;
+    } 
+    if (generateTetrimino()) {
+        return generate_piece;
+    }
+    if (isGameOver(board_values, empty_block)) {
+        return game_over;
+    }
 
-
-    // int count_part1 = 0;
-    // int count_part2 = 1;
-
-    // // #
-    // //###
-    // board_values[0][5] = '1';
-    // board_values[1][4] = object_block;
-    // board_values[1][5] = object_block;
-    // board_values[1][6] = object_block;
-
-    //printf(object_block);
-    //printf(object_block["key"].as<std::string>());
-//std::cout << object_block["key"].as<std::string>(); 
-
-    // tetrimino[count_part1][5] = 0;
-    // tetrimino[count_part2][4] = 0;
-    // tetrimino[count_part2][5] = 0;
-    // tetrimino[count_part2][6] = 0;
-
-    // count_part1++;
-    // count_part2++;
-    // tetrimino[count_part1][5] = 1;
-    // tetrimino[count_part2][4] = 1;
-    // tetrimino[count_part2][5] = 1;
-    // tetrimino[count_part2][6] = 1;
-
-    // for (int row = 0; row < 20; row++) {
-    //     for (int col = 0; col < 10; col++) {
-    //         if (tetrimino[row][col] == 1) {
-    //             gameBoard[row][col] = tetrimino[row][col];
-    //         }
-    //         if (gameBoard[row][col] == 1 && tetrimino[row][col] == 0) {
-    //             gameBoard[row][col] = 0;
-    //         }
-    //     }
-    // }
-     return no_winner_label;
 }
-
 
 EMSCRIPTEN_BINDINGS(my_module) {
     function("tetris", &tetris);
--- main.js
+++ main.js
@@ -1150,9 +1150,9 @@ function updateGlobalBufferAndViews(buf) {
   Module['HEAPF64'] = HEAPF64 = new Float64Array(buf);
 }
 
-var STACK_BASE = 5248896,
+var STACK_BASE = 5248944,
     STACKTOP = STACK_BASE,
-    STACK_MAX = 6016;
+    STACK_MAX = 6064;
 
 assert(STACK_BASE % 16 === 0, 'stack must start aligned');
 
